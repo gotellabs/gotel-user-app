b9ae32e82c25b61d4a65c1cea72ad69c45418daf
Enzo Lizama <lizama.enzo@gmail.com>
2020-04-02 20:21:11 -0500

    :robot: add splash activity
    Signed-off-by: Enzo Lizama <lizama.enzo@gmail.com>

---
+++ app/src/main/java/com/gotellabs/gotel/ui/splash/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.gotellabs.gotel.ui.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gotellabs.gotel.R
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
