fe33c9352b11a8efd4ed13a59e52a98c3d3a1549
Enzo Lizama <lizama.enzo@gmail.com>
2020-04-02 20:19:25 -0500

    :robot: add boarding activity
    Signed-off-by: Enzo Lizama <lizama.enzo@gmail.com>

---
+++ app/src/main/java/com/gotellabs/gotel/ui/boarding/BoardingActivity.kt
@@ -0,0 +1,13 @@
+package com.gotellabs.gotel.ui.boarding
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gotellabs.gotel.R
+
+class BoardingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_boarding)
+    }
+}
