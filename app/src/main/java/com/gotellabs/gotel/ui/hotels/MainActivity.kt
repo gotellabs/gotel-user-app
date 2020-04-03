dade338b3dba6d1a62341953c41a83bc2939431e
Enzo Lizama <lizama.enzo@gmail.com>
2020-04-02 20:09:26 -0500

    :robot: Work with object hotel from domain module

--- app/src/main/java/com/gotellabs/gotel/ui/hotels/MainActivity.kt
+++ app/src/main/java/com/gotellabs/gotel/ui/hotels/MainActivity.kt
@@ -2,12 +2,16 @@ package com.gotellabs.gotel.ui.hotels
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gotellabs.domain.HotelModel
 import com.gotellabs.gotel.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val hotel = HotelModel(id = 1, name = "Hotel1", description = "Hello")
+        hello_text.text = hotel.description
     }
 }
