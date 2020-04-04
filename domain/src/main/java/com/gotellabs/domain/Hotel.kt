16846eb7bd203df10ccafc023898a90e15ad6362
Enzo Lizama <lizama.enzo@gmail.com>
2020-04-03 21:27:05 -0500

    :busts_in_silhouette: adding models for project
    Signed-off-by: Enzo Lizama <lizama.enzo@gmail.com>

--- domain/src/main/java/com/gotellabs/domain/Hotel.kt
+++
@@ -1,3 +0,0 @@
-package com.gotellabs.domain
-
-data class HotelModel(val id: Int, val name: String, val description: String)
---
+++ domain/src/main/java/com/gotellabs/domain/model/HotelModel.kt
@@ -0,0 +1,3 @@
+package com.gotellabs.domain.model
+
+data class HotelModel(val id: Int, val name: String, val description: String, val image: String)
