d4e14bf760cfa5d355c913995fed8143af0c3d12
Enzo Lizama <lizama.enzo@gmail.com>
2020-04-03 21:25:47 -0500

    :ok_hand: adding data module
    Signed-off-by: Enzo Lizama <lizama.enzo@gmail.com>

---
+++ data/src/main/java/com/example/upc/data/mock/MockData.kt
@@ -0,0 +1,15 @@
+package com.example.upc.data.mock
+
+import com.gotellabs.domain.model.HotelModel
+
+class MockData {
+
+    val hotel = HotelModel(
+        id = 1,
+        name = "Hotel1",
+        description = "Hello",
+        image = "https://www.perutravelling.com/images/lima/hotel-carmel-lima.jpg"
+    )
+
+
+}
