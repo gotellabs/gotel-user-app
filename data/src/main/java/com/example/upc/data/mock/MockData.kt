f32be4f29b09576ad19339e4496b049437c31588
Enzo Lizama <lizama.enzo@gmail.com>
2020-04-03 21:32:11 -0500

    :clown_face: mocking data
    Signed-off-by: Enzo Lizama <lizama.enzo@gmail.com>

--- data/src/main/java/com/example/upc/data/mock/MockData.kt
+++ data/src/main/java/com/example/upc/data/mock/MockData.kt
@@ -7,7 +7,7 @@ class MockData {
     val hotel = HotelModel(
         id = 1,
         name = "Hotel1",
-        description = "Hello",
+        description = "Hello hotels",
         image = "https://www.perutravelling.com/images/lima/hotel-carmel-lima.jpg"
     )
 
