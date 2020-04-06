95f6bcaa02a0b068e99a9dfa75e25ab3070d3255
Enzo Lizama <lizama.enzo@gmail.com>
2020-04-06 09:57:52 -0500

    :wrench: data kotlin module to android module for room
    Signed-off-by: Enzo Lizama <lizama.enzo@gmail.com>

--- data/src/main/java/com/example/upc/data/mock/MockData.kt
+++ data/src/main/java/com/example/upc/data/mock/MockData.kt
@@ -4,25 +4,30 @@ import com.gotellabs.domain.model.HotelModel
 
 class MockData {
 
-    val mockHotelList = listOf(
-        HotelModel(
-            id = 1,
-            name = "Hotel1",
-            description = "Hello hotels",
-            image = "https://www.perutravelling.com/images/lima/hotel-carmel-lima.jpg"
-        ),
-        HotelModel(
-            id = 2,
-            name = "Hotel2",
-            description = "Hello hotels 2",
-            image = "https://www.perutravelling.com/images/lima/hotel-carmel-lima.jpg"
-        ),
-        HotelModel(
-            id = 3,
-            name = "Hotel3",
-            description = "Hello hotels 3",
-            image = "https://www.perutravelling.com/images/lima/hotel-carmel-lima.jpg"
+    companion object {
+        val mockHotelList = listOf(
+            HotelModel(
+                id = 1,
+                name = "Hotel1",
+                description = "Hello hotels",
+                image = "https://www.perutravelling.com/images/lima/hotel-carmel-lima.jpg"
+            ),
+            HotelModel(
+                id = 2,
+                name = "Hotel2",
+                description = "Hello hotels 2",
+                image = "https://www.perutravelling.com/images/lima/hotel-carmel-lima.jpg"
+            ),
+            HotelModel(
+                id = 3,
+                name = "Hotel3",
+                description = "Hello hotels 3",
+                image = "https://www.perutravelling.com/images/lima/hotel-carmel-lima.jpg"
+            )
         )
-    )
+
+    }
+
+
 
 }
