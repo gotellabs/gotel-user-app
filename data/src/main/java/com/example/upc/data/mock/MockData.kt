c6cb2c0cf80809db917a48d707612db1a4fd86c3
Enzo Lizama <lizama.enzo@gmail.com>
2020-04-06 00:16:02 -0500

    :clown_face: mock data on list
    Signed-off-by: Enzo Lizama <lizama.enzo@gmail.com>

--- data/src/main/java/com/example/upc/data/mock/MockData.kt
+++ data/src/main/java/com/example/upc/data/mock/MockData.kt
@@ -4,12 +4,25 @@ import com.gotellabs.domain.model.HotelModel
 
 class MockData {
 
-    val hotel = HotelModel(
-        id = 1,
-        name = "Hotel1",
-        description = "Hello hotels",
-        image = "https://www.perutravelling.com/images/lima/hotel-carmel-lima.jpg"
+    val mockHotelList = listOf(
+        HotelModel(
+            id = 1,
+            name = "Hotel1",
+            description = "Hello hotels",
+            image = "https://www.perutravelling.com/images/lima/hotel-carmel-lima.jpg"
+        ),
+        HotelModel(
+            id = 2,
+            name = "Hotel2",
+            description = "Hello hotels 2",
+            image = "https://www.perutravelling.com/images/lima/hotel-carmel-lima.jpg"
+        ),
+        HotelModel(
+            id = 3,
+            name = "Hotel3",
+            description = "Hello hotels 3",
+            image = "https://www.perutravelling.com/images/lima/hotel-carmel-lima.jpg"
+        )
     )
-
 
 }
