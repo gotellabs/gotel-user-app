3cd32a00f6a9e4a41570ecec76bf4c703e2838ca
Enzo Lizama <lizama.enzo@gmail.com>
2020-05-08 21:02:35 -0500

    fix id to string
    Signed-off-by: Enzo Lizama <lizama.enzo@gmail.com>

--- domain/src/main/java/com/gotellabs/domain/model/HotelModel.kt
+++ domain/src/main/java/com/gotellabs/domain/model/HotelModel.kt
@@ -3,10 +3,10 @@ package com.gotellabs.domain.model
 import java.io.Serializable
 
 data class HotelModel(
-    val _id: Int,
+    val _id: String,
     val name: String,
     val description: String,
-    val image: String? = "hi",
+    val image: String? = "https://www.ahstatic.com/photos/9399_ho_00_p_1024x768.jpg",
     val phone: String? = "97012921",
     val rate: String? = "5.0"
 ) : Serializable
