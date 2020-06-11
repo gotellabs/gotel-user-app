3bdf4aeada8a681ebd1caaf1436d8afdc1476093
Enzo Lizama <lizama.enzo@gmail.com>
2020-06-11 01:50:00 -0500

    fix image in hotel feed
    Signed-off-by: Enzo Lizama <lizama.enzo@gmail.com>

--- app/src/main/java/com/gotellabs/gotel/ui/feed/HotelsViewHolder.kt
+++ app/src/main/java/com/gotellabs/gotel/ui/feed/HotelsViewHolder.kt
@@ -1,6 +1,7 @@
 package com.gotellabs.gotel.ui.feed
 
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.gotellabs.domain.model.Hotel
 import com.gotellabs.gotel.databinding.HotelItemBinding
 
@@ -20,16 +21,15 @@ class HotelsViewHolder(private val binding: HotelItemBinding) :
     ) {
         binding.hotelName.text = hotel.name
         binding.hotelDescription.text = hotel.description
-//        if(hotel.hotelPhotos.isEmpty()){
-//            Glide.with(binding.hotelItemImageView.context)
-//                .load("https://www.ahstatic.com/hotelPhotos/9399_ho_00_p_1024x768.jpg")
-//                .into(binding.hotelItemImageView)
-//        }
-//        else{
-//            Glide.with(binding.hotelItemImageView.context)
-//                .load(hotel.hotelPhotos[0].imageUrl)
-//                .into(binding.hotelItemImageView)
-//        }
+        if (hotel.hotelPhotos.isEmpty()) {
+            Glide.with(binding.hotelItemImageView.context)
+                .load("https://www.ahstatic.com/hotelPhotos/9399_ho_00_p_1024x768.jpg")
+                .into(binding.hotelItemImageView)
+        } else {
+            Glide.with(binding.hotelItemImageView.context)
+                .load(hotel.hotelPhotos[0].imageUrl)
+                .into(binding.hotelItemImageView)
+        }
         binding.hotelCardView.setOnClickListener {
             onItemClickListener?.onItemClicked(hotel)
         }
