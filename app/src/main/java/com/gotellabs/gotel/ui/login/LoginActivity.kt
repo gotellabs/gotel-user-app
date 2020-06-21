1f8886ff1d11923576a9746f496df26d951f6475
RodrigoAGM <rod-gm@hotmail.com>
2020-06-21 11:20:19 -0500

    Add files to gitingnore

--- app/src/main/java/com/gotellabs/gotel/ui/login/LoginActivity.kt
+++ app/src/main/java/com/gotellabs/gotel/ui/login/LoginActivity.kt
@@ -2,21 +2,16 @@ package com.gotellabs.gotel.ui.login
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
 
 import com.gotellabs.gotel.R
-import com.gotellabs.gotel.base.BaseActivity
 import com.gotellabs.gotel.ui.feed.HotelsActivity
 import kotlinx.android.synthetic.main.activity_login.*
 
